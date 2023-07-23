683ff365d18318d4b42e550088d1a6aec5db1d59
Petr Khasanov <n0sfer666@yandex.ru>
2023-07-23 13:29:53 +0300

    HOK; var vs state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [a, setA] = useState(0);
+
+  let b = 0;
+
+  useEffect(() => {
+    console.log('a changed, now is: ', a);
+    console.log('b is: ', b);
+  }, [a, b])
+
+  useEffect(() => {
+    console.log('b changed, now is:', b)
+  }, [b])
+
+  const handleCLickState = () => { setA(a+1) }
+  const handleClick = () => { b += 1 }
+
+  const handleJustClick = () => { console.log('click') }
+
+  const renderComponentWithHandler = (
+    stateHandler = handleJustClick,
+    badHandler = handleJustClick) => (<>
+      <button onClick={stateHandler}>click me state</button>
+      <br />
+      <button onClick={badHandler}>click me</button>
+    </>)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          {renderComponentWithHandler(handleCLickState, handleClick)}
+        </div>
+        <div>
+        {renderComponentWithHandler()}
+        </div>
       </header>
     </div>
   );
+
 }
 
 export default App;
